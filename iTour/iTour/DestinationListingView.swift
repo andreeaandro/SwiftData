fa7405ea447022a0c9779b7c43ad5cb3635bb754
Andreea Andro <andreeaandro19@gmail.com>
2024-01-30 13:30:37 +0200

    hackingwithswift quick-start swiftdata

---
+++ iTour/iTour/DestinationListingView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+import SwiftData
+
+struct DestinationListingView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: [SortDescriptor(\Destination.priority, order: .reverse), SortDescriptor(\Destination.name)]) var destinations: [Destination]
+    
+    var body: some View {
+        List {
+            ForEach(destinations) { destination in
+                NavigationLink(value: destination) {
+                    VStack(alignment: .leading, content: {
+                        Text(destination.name)
+                            .font(.headline)
+                        Text(destination.date.formatted())
+                    })
+                }
+            }
+            .onDelete(perform: deleteDestinations)
+        }
+    }
+    
+    func deleteDestinations(_ indexSet: IndexSet) {
+        for index in indexSet {
+            let destination = destinations[index]
+            modelContext.delete(destination)
+        }
+    }
+    
+    init(sort: SortDescriptor<Destination>, searchString: String) {
+        _destinations = Query(filter: #Predicate {
+            if searchString.isEmpty {
+                return true
+            } else {
+                return $0.name.localizedStandardContains(searchString)
+            }
+        }, sort: [sort])
+    }
+}
+
+#Preview {
+    DestinationListingView(sort: SortDescriptor(\Destination.name), searchString: "")
+}
---
+++ iTour/iTour/Sight.swift
@@ -0,0 +1,11 @@
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
